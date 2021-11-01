e0aa3de406cf8da521f2561ca9a65ff43cd84f27
zejji <jezhl@yahoo.co.uk>
2021-11-01 06:55:22 +0000

    Add example Md5VsSha256 benchmark class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+using InterfacePerfTest;
+using System.Diagnostics;
+
+// See https://aka.ms/new-console-template for more information
+var summary = BenchmarkRunner.Run<Md5VsSha256>();
+Debug.WriteLine(summary);
