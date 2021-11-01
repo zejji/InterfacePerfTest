5b66c70a8e2f2473cbdedb271a3e6cfa356daf2c
zejji <jezhl@yahoo.co.uk>
2021-11-01 07:01:21 +0000

    WIP on adding new classes to test a compilation generated via Roslyn

---
+++ CompilationBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePerfTest
+{
+    internal class CompilationBuilder
+    {
+        public Compilation CreateCompilation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
