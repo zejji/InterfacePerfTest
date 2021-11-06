c30565175c5de61cf094f0d1dbc7326af4b01481
zejji <jezhl@yahoo.co.uk>
2021-11-06 14:23:32 +0000

    Example of compiling source text to in-memory assembly and calling a method

--- CompilationBuilder.cs
+++ CompilationBuilder.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.CodeAnalysis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace InterfacePerfTest
 {
@@ -11,7 +10,69 @@ namespace InterfacePerfTest
     {
         public Compilation CreateCompilation()
         {
-            throw new NotImplementedException();
+            var source = _createTestSource();
+            
+            var coreLibReference = MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location);
+            var netstandardReference = MetadataReference.CreateFromFile(Assembly.Load("netstandard, Version=2.0.0.0").Location);
+            var runtimeReference = MetadataReference.CreateFromFile(Assembly.Load("System.Runtime, Version=6.0.0.0").Location);
+            var systemReference = MetadataReference.CreateFromFile(typeof(Console).GetTypeInfo().Assembly.Location);
+
+            var additionalReferences = new[] { coreLibReference, netstandardReference, runtimeReference, systemReference };
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName: "compilation",
+                syntaxTrees: new[] { CSharpSyntaxTree.ParseText(source) },
+                references: additionalReferences,
+                options: new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+
+            // Require that no serious errors occurred during compilation
+            IEnumerable<Diagnostic> failures = _getErrorDiagnostics(compilation);
+            foreach (Diagnostic diagnostic in failures)
+            {
+                Debug.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+            }
+
+            if (failures.Any()) throw new Exception();
+
+            return compilation;
+        }
+
+        private static string _createTestSource()
+        {
+            return @"
+using System;
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var myClass = new HelloWorld();
+            myClass.SayHello();
+        }
+    }
+
+    public class HelloWorld
+    {
+        public void SayHello() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+        }
+    }
+}
+";
+        }
+
+        private static IEnumerable<Diagnostic> _getErrorDiagnostics(Compilation inputCompilation)
+        {
+            ImmutableArray<Diagnostic> compilationDiagnostics = inputCompilation.GetDiagnostics();
+
+            IEnumerable<Diagnostic> failures = compilationDiagnostics.Where(diagnostic =>
+                        diagnostic.Id != "CS5001" && // i.e. "Program does not contain a static 'Main' method suitable for an entry point"
+                        (diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error));
+
+            return failures;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,65 @@
 ﻿using BenchmarkDotNet.Running;
 using InterfacePerfTest;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Emit;
 using System.Diagnostics;
+using System.Reflection;
 
-// See https://aka.ms/new-console-template for more information
-var summary = BenchmarkRunner.Run<Md5VsSha256>();
-Debug.WriteLine(summary);
+//// See https://aka.ms/new-console-template for more information
+//var summary = BenchmarkRunner.Run<Md5VsSha256>();
+//Debug.WriteLine(summary);
+
+var compilationBuilder = new CompilationBuilder();
+var compilation = compilationBuilder.CreateCompilation();
+
+Assembly? assembly = null;
+
+// Create an in-memory assembly from the compilation.
+// https://www.tugberkugurlu.com/archive/compiling-c-sharp-code-into-memory-and-executing-it-with-roslyn
+using (var ms = new MemoryStream())
+{
+    EmitResult result = compilation.Emit(ms);
+
+    if (!result.Success)
+    {
+        IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+            diagnostic.IsWarningAsError ||
+            diagnostic.Severity == DiagnosticSeverity.Error);
+
+        foreach (Diagnostic diagnostic in failures)
+        {
+            Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+        }
+    }
+    else
+    {
+        ms.Seek(0, SeekOrigin.Begin);
+        assembly = Assembly.Load(ms.ToArray());
+    }
+}
+
+if (assembly == null)
+{
+    throw new InvalidOperationException($"{nameof(assembly)} is null.");
+}
+
+Type? type = assembly.GetType("HelloWorld.HelloWorld");
+
+if (type == null)
+{
+    throw new InvalidOperationException($"{nameof(type)} is null.");
+}
+
+object? obj = Activator.CreateInstance(type);
+
+if (obj == null)
+{
+    throw new InvalidOperationException($"{nameof(obj)} is null.");
+}
+
+type.InvokeMember("SayHello",
+    BindingFlags.Default | BindingFlags.InvokeMethod,
+    null,
+    obj,
+    null//new object[] { "Hello World" }
+    );
